c3fdac98f881982b7c9d05f9b8fccf4a9b2d3116
chie.ichikawa <orangepeco1811@hotmail.co.jp>
2018-02-24 12:24:47 +0000

    kadai-tasklist up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+   def index
+     @tasks = Task.all
+  end
+
+  def show
+     @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
