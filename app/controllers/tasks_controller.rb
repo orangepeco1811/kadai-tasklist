70a52331f2b824527c40de3e4379fdcb18fcd40d
chie.ichikawa <orangepeco1811@hotmail.co.jp>
2018-03-08 15:05:35 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,13 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
-    @tasks = Task.all.page(params[:page])
+     if logged_in?
+      @user = current_user
+      @task = current_user.tasks.build  # form_for 用
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+     end
   end
   
   def show
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
-    belongs_to :user
+  belongs_to :user
 
   validates :content, presence: true, length: { maximum: 10 }
   validates :status, presence: true, length: { maximum: 255 }
+  
 end
